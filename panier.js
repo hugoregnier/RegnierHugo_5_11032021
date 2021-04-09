0df4259f2c2ac40358f9992007cb3eb1304b3f10
Hugo REGNIER <hma.regnier@orange.fr>
2021-04-09 14:40:23 +0200

    prix total + formulaire + début regex

--- panier.js
+++ panier.js
@@ -1,7 +1,9 @@
-let panier = JSON.parse(localStorage.getItem("panier"));
+let panier = JSON.parse(localStorage.getItem("panier")); //----  On initialise à chaque fois notre localStorage ----//
 if (!panier) {
     panier = new Array();
 }
+
+let total = 0;
 
 for (let i in panier) {
 
@@ -16,6 +18,7 @@ for (let i in panier) {
     let price = document.getElementById('prix');
     let prix = document.createElement('h2');
     prix.innerText = panier[i].price + (' €');
+    total += panier[i].price; //----  Prix total avec += / le parseInt transforme en nombre sans virgule ----//
 
     let supprime = document.getElementById('supprimer');
     let button = document.createElement('button');
@@ -31,12 +34,36 @@ for (let i in panier) {
         localStorage.setItem('panier', JSON.stringify(panier)); //---- On remet à jour le localStorage après une suppression ----//
     });
 
+
     name.appendChild(title);
     focale.appendChild(optique);
     price.appendChild(prix);
     supprime.appendChild(button);
 
+
 }
 
-console.log(panier);
+let totalPrice = document.getElementById('totalPanier');
+totalPrice.innerText = total + " €";
+//----fonction Regex : nom,prénom,ville / expression régulière ----//
+function validateText(value) {
+    return /[a-zA-Z]+/.test(value);
+}
+//----fonction Regex : adresse / expression régulière ----//
+function validateAdresse(value) {
+    return /\w+/.test(value);
+}
+//----fonction Regex : email   / expression régulière ----//
+function validateEmail(value) {
+    var emailReg = new RegExp(/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i);
+    var valid = emailReg.test(value);
+
+    if (!valid) {
+        return false;
+    } else {
+        return true;
+    }
+}
+//---- button de validation de commande(fonction / if) ----//
+
 
