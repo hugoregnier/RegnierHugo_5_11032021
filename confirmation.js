9449e9582bd61c3312bdc357645719d0daa132fe
Hugo REGNIER <hma.regnier@orange.fr>
2021-04-16 13:36:04 +0200

    total in confirmation

--- confirmation.js
+++ confirmation.js
@@ -1,5 +1,21 @@
+let panier = JSON.parse(localStorage.getItem("panier")); //----  On initialise à chaque fois notre localStorage ----//
 let orderId = window.location.search.substr(1).split("=")[1];
+let total = 0;
+
+for (let i in panier) {  //---- refait une boucle for pour le total----//
+    total += panier[i].price;
+}
+
 
 let content = document.getElementById('content');
- content.innerText = orderId;
+content.innerText = 'Nous vous remercions pour votre commande N°' + orderId;
+ 
+let totalPrice = document.getElementById('totalPanier');
+
+console.log(panier)
+totalPrice.innerText = "Montant total de votre commande " + total + " €";
+
+
+localStorage.setItem('panier', JSON.stringify([]));  //---- on remet a jour le panier ----//
+
 
